7180b8c12147448cb017fbbb6899499b8f42186f
nick <nick889547@qq.com>
2018-10-17 14:04:04 +0800

    first commit

---
+++ app/src/main/java/com/example/user_pc/myweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.user_pc.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
